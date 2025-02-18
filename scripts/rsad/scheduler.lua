aa5708a8a60a84dea2a8670d6784b12cddd32542
Amber <aqscode@gmail.com>
2025-02-18 13:37:01 -0700

    Create scheduler - limited

---
+++ scripts/rsad/scheduler.lua
@@ -0,0 +1,18 @@
+require("scripts.rsad.station")
+require("scripts.rsad.rsad-controller")
+
+-- TODO: Expand to inter-yard deliveries
+
+---@param station RSADStation
+---@return boolean, uint?, uint? --- Whether or not the request was successful, unit_number for the train assigned to delivery (nil if failed), error number (nil if successful) 
+function handle_station_request(station)
+    if not station then return false, nil, 3 end
+    
+    local yard = create_or_get_train_yard(station.network) ---@type TrainYard
+    local request = station.item
+    local assigned_train ---@type uint
+
+    if not yard or #yard.shunting_depots == 0 or not yard.import_stations[request] then return false, nil, 2 end
+
+    return true, assigned_train
+end
